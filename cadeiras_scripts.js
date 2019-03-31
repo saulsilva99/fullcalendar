4acd6cfb8b510814fae97737eb09109e11357ac1
Saul Kevin Ataíde Silva <fc44301@alunos.fc.ul.pt>
2019-03-31 00:04:17 +0000

    Ja adiciona as cadeiras ao calendário, mas repete se fizermos varias vezes se eu quiser.

--- cadeiras_scripts.js
+++ cadeiras_scripts.js
@@ -1,42 +1,53 @@
-$(function() {
-  $('#calendar').fullCalendar({
-    plugins: ['interaction', 'timeGrid'],
-    header: {
-      left: 'prev,next today',
-      center: 'title',
-      right: 'timeGridWeek',
-    },
-    buttonText: {
-      today: "Hoje",
-      week: "Semana",
-    },
-    defaultView: 'timeGridWeek',
-    allDayText: "Horas",
-    hiddenDays: [0, 6],
-    minTime: "08:00:00",
-    maxTime: "20:00:00",
-    locale: 'pt',
-    defaultDate: '2019-03-29',
-    navLinks: true, // can click day/week names to navigate views
-    businessHours: true, // display business hours
-    editable: true,
-    eventLimit: true,
-    events: [{
-      title: 'Saul The best',
-      start: '2019-03-29T13:00:00',
-      end: '2019-03-29T14:00:00',
-      constraint: 'businessHours'
-    }]
-  });
-});
-function evento(name) {
+$(document).ready(function() {
 
-  var myEvent = {
-    title: name,
-    start: '2019-03-26T13:00:00',
-    end: '2019-03-26T14:00:00',
-    constraint: 'businessHours'
-  };
-  var calendar = $('#calendar');
-  calendar.fullCalendar('renderEvent', myEvent);
+  $('#calendar').fullCalendar({
+    plugins: [ 'interaction','timeGrid' ],
+    defaultView: 'agendaWeek',
+    header: {
+      left: 'prev,next,today',
+      center: 'title',
+      right: 'agendaWeek'
+    },
+      allDayText: "Horas",
+       hiddenDays: [0, 6],
+       minTime: "08:00:00",
+       maxTime: "20:00:00",
+       locale: 'pt',
+       defaultDate: '2019-03-29',
+       navLinks: true, // can click day/week names to navigate views
+       editable: false,
+       events: [{
+          id:1,
+          title: 'Saul The best',
+          start: '2019-03-29T13:00:00',
+          end: '2019-03-29T14:00:00',
+       }]
+  });
+
+});
+
+function evento(name) {
+  var eventoo =  handlerEventNames(name);
+  $('#calendar').fullCalendar('renderEvent', eventoo);
+
+}
+function handlerEventNames(name){
+  console.log(name);
+  if(name == 'ads-T21'){
+    var eventoo = {id:2,title: name,start: '2019-03-26T14:30:00',end: '2019-03-26T15:30:00',
+    };
+    return eventoo;
+  }
+  else if(name == 'ads-T22'){
+    var eventoo = {id:3,title: name,start: '2019-03-26T13:00:00',end: '2019-03-26T14:00:00',
+    };
+    return eventoo;
+  }
+  else if(name == 'ads-TP21'){
+    var eventoo = {id:4,title: name,start: '2019-03-26T13:00:00',end: '2019-03-26T14:00:00',
+    };
+    return eventoo;
+  }
+
+
 }
